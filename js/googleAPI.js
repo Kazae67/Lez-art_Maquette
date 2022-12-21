20ce9a468a63122fea519d2fa35e1026613f916b
Kazae67 <Kazae.zaeka@gmail.com>
2022-12-21 10:41:21 +0100

    ajout formulaire début

---
+++ js/googleAPI.js
@@ -0,0 +1,17 @@
+{/* <div id="map"></div>
+
+<script>
+  function initMap() {
+    var position = {lat: 48.864716, lng: 2.349014};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: position
+    });
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map
+    });
+  }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+async defer></script> */}
