4afe02361337faa2e6829d90d4799fedaebe9977
Kazae67 <Kazae.zaeka@gmail.com>
2022-12-22 14:36:57 +0100

    sauvegarde

--- js/formulaire.js
+++ js/formulaire.js
@@ -1,15 +1,15 @@
 
-  // Récupération de tous les boutons "Réserver"
-  const buyButtons = document.querySelectorAll('.buy');
+//   // Récupération de tous les boutons "Réserver"
+//   const buyButtons = document.querySelectorAll('.buy');
   
-  // Ajout de l'événement "click" sur chaque bouton
-  buyButtons.forEach(button => {
-    button.addEventListener('click', event => {
-      // Récupération du formulaire correspondant au bouton cliqué
-      const form = event.target.closest('.product-details').nextElementSibling.querySelector('.reservation-form');
+//   // Ajout de l'événement "click" sur chaque bouton
+//   buyButtons.forEach(button => {
+//     button.addEventListener('click', event => {
+//       // Récupération du formulaire correspondant au bouton cliqué
+//       const form = event.target.closest('.product-details').nextElementSibling.querySelector('.reservation-form');
       
-      // Afficher le formulaire en modifiant la propriété "display"
-      form.style.display = 'block';
-    });
-  });
+//       // Afficher le formulaire en modifiant la propriété "display"
+//       form.style.display = 'block';
+//     });
+//   });
 
